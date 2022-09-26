faba41510d9da9c47d0545387837fa4b92d3b431
toshiya matsukawa <matsukawa5955@gmail.com>
2022-09-26 16:39:26 +0300

    symbolOpenApi

---
+++ SymbolOpenApi/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Main
+{
+    public void Program()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
